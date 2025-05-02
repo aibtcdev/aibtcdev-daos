bd24e088675ad4f9bc2277d8f791195d16bf8039
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-05-02 11:27:33 -0700

    feat: add deployment order export

---
+++ tests/utilities/contract-deployment-order.ts
@@ -0,0 +1,22 @@
+export const DEPLOYMENT_ORDER = {
+  // token contracts
+  "aibtc-pre-faktory": 10,
+  "aibtc-faktory": 11,
+  "xyk-pool-sbtc-aibtc-v-1-1": 12,
+  "aibtc-faktory-dex": 13,
+  // base dao contract
+  "aibtc-base-dao": 20,
+  // extensions
+  "aibtc-action-proposal-voting": 30,
+  "aibtc-dao-charter": 31,
+  "aibtc-dao-epoch": 32,
+  "aibtc-dao-users": 33,
+  "aibtc-onchain-messaging": 34,
+  "aibtc-rewards-account": 35,
+  "aibtc-token-owner": 36,
+  "aibtc-treasury": 37,
+  // actions
+  "aibtc-action-send-message": 40,
+  // initialize dao (always last)
+  "aibtc-initialize-dao": 50,
+};
