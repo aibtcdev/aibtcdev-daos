bae80054e37d883b26ba67e301b47a37014457be
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-05-12 09:43:26 -0700

    fix: use correct fs/path imports

--- utilities/template-processor.ts
+++ utilities/template-processor.ts
@@ -1,6 +1,6 @@
 import { dbgLog } from "./debug-logging";
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
 
 /**
  * Processes a contract template line by line and performs replacements
