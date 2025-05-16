32ae39821a820fc0e2be0d86d712bd11f6973c58
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-05-16 10:52:32 -0700

    fix: restore the ignore for dist/

--- @aibtc/types/dist/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * AIBTC DAO API Types
- *
- * This package provides TypeScript type definitions for the AIBTC DAO API.
- */
-import { CONTRACT_NAMES } from "../../../utilities/contract-types";
-export { CONTRACT_NAMES };
