f1ad5296abf82cafa3b71396c580d3a5dafbdc23
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-07-14 15:15:13 -0700

    fix: add basic structure for test, pull from registry

--- tests/contracts/dao/trading/aibtc-acct-swap-bitflow-aibtc-sbtc.test.ts
+++ tests/contracts/dao/trading/aibtc-acct-swap-bitflow-aibtc-sbtc.test.ts
@@ -1,12 +1,39 @@
-// TESTS WE NEED
+import { describe } from "vitest";
+import { setupFullContractRegistry } from "../../../../utilities/contract-registry";
 
-// buy-dao-token() fails if token contract does not match
-// buy-dao-token() fails if minReceive is not provided
-// buy-dao-token() succeeds and swaps for tx-sender
+// setup contract info for tests
+const registry = setupFullContractRegistry();
+const contractAddress = registry.getContractAddressByTypeAndSubtype(
+  "TRADING",
+  "BITFLOW_SBTC"
+);
+const contractName = contractAddress.split(".")[1];
 
-// sell-dao-token() fails if token contract does not match
-// sell-dao-token() fails if minReceive is not provided
-// sell-dao-token() succeeds and swaps for tx-sender
+// DAO contract references
+const daoTokenAddress = registry.getContractAddressByTypeAndSubtype(
+  "TOKEN",
+  "DAO"
+);
 
-// get-contract-info() returns correct contract info
-// get-swap-info() returns correct swap info
+// import error codes
+// TODO
+
+describe(`public functions: ${contractName}`, () => {
+  ////////////////////////////////////////
+  // buy-dao-token() tests
+  ////////////////////////////////////////
+  // buy-dao-token() fails if token contract does not match
+  // buy-dao-token() fails if minReceive is not provided
+  // buy-dao-token() succeeds and swaps for tx-sender
+  ////////////////////////////////////////
+  // sell-dao-token() tests
+  ////////////////////////////////////////
+  // sell-dao-token() fails if token contract does not match
+  // sell-dao-token() fails if minReceive is not provided
+  // sell-dao-token() succeeds and swaps for tx-sender
+});
+
+describe(`read-only functions: ${contractName}`, () => {
+  // get-contract-info() returns correct contract info
+  // get-swap-info() returns correct swap info
+});
--- tests/contracts/dao/trading/aibtc-acct-swap-faktory-aibtc-sbtc.test.ts
+++ tests/contracts/dao/trading/aibtc-acct-swap-faktory-aibtc-sbtc.test.ts
@@ -1,14 +1,41 @@
-// TESTS WE NEED
+import { describe } from "vitest";
+import { setupFullContractRegistry } from "../../../../utilities/contract-registry";
 
-// buy-dao-token() fails if token contract does not match
-// buy-dao-token() fails if minReceive is provided and slippage too high
-// buy-dao-token() succeeds and swaps for tx-sender without minReceive
-// buy-dao-token() succeeds and swaps for tx-sender with minReceive
+// setup contract info for tests
+const registry = setupFullContractRegistry();
+const contractAddress = registry.getContractAddressByTypeAndSubtype(
+  "TRADING",
+  "FAKTORY_SBTC"
+);
+const contractName = contractAddress.split(".")[1];
 
-// sell-dao-token() fails if token contract does not match
-// buy-dao-token() fails if minReceive is provided and slippage too high
-// sell-dao-token() succeeds and swaps for tx-sender without minReceive
-// sell-dao-token() succeeds and swaps for tx-sender with minReceive
+// DAO contract references
+const daoTokenAddress = registry.getContractAddressByTypeAndSubtype(
+  "TOKEN",
+  "DAO"
+);
 
-// get-contract-info() returns correct contract info
-// get-swap-info() returns correct swap info
+// import error codes
+// TODO
+
+describe(`public functions: ${contractName}`, () => {
+  ////////////////////////////////////////
+  // buy-dao-token() tests
+  ////////////////////////////////////////
+  // buy-dao-token() fails if token contract does not match
+  // buy-dao-token() fails if minReceive is provided and slippage too high
+  // buy-dao-token() succeeds and swaps for tx-sender without minReceive
+  // buy-dao-token() succeeds and swaps for tx-sender with minReceive
+  ////////////////////////////////////////
+  // sell-dao-token() tests
+  ////////////////////////////////////////
+  // sell-dao-token() fails if token contract does not match
+  // sell-dao-token() fails if minReceive is provided and slippage too high
+  // sell-dao-token() succeeds and swaps for tx-sender without minReceive
+  // sell-dao-token() succeeds and swaps for tx-sender with minReceive
+});
+
+describe(`read-only functions: ${contractName}`, () => {
+  // get-contract-info() returns correct contract info
+  // get-swap-info() returns correct swap info
+});
